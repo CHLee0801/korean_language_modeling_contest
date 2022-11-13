8f80ef9156217e67df4d0494e54ad9367cca6a7d
CHLee0801 <ckdgh0801@korea.ac.kr>
2022-11-13 15:04:48 +0900

    fixed untracked files

--- run.sh
+++
@@ -1,4 +0,0 @@
-#python run.py --config configs/bert/bert_3way.json
-#python run.py --config configs/bert/bert_category.json
-python run.py --config configs/bert/bert_topic.json
-python run.py --config configs/bert/bert_sentiment.json
